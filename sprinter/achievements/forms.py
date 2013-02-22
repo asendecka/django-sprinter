b8d32e70e50a0e74e1e05aca3073896356deca92
Aleksandra Sendecka <asendecka@hauru.eu>
2013-02-22 20:43:16 +0100

    Gravatar is not required.

--- sprinter/achievements/forms.py
+++ sprinter/achievements/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from sprinter.achievements.models import Sprinter
 
 class SprinterForm(forms.ModelForm):
-    gravatar_email = forms.EmailField(label="Gravatar email")
+    gravatar_email = forms.EmailField(label="Gravatar email", required=False)
 
     def __init__(self, *args, **kwargs):
         super(SprinterForm, self).__init__(*args, **kwargs)
