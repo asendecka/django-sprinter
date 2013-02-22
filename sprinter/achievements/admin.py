a71ce1fdbe5d6ae7a2ffe984ed8e95cee12a7875
Aleksandra Sendecka <asendecka@hauru.eu>
2013-02-23 00:48:48 +0100

    Sprinter added to admin.

--- sprinter/achievements/admin.py
+++ sprinter/achievements/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from sprinter.achievements.models import Achievement
+from sprinter.achievements.models import Achievement, Sprinter
 
 class AchievementAdmin(admin.ModelAdmin):
     pass
 
+class SprinterAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Achievement, AchievementAdmin)
+admin.site.register(Sprinter, SprinterAdmin)
