657e0c6586260abe95f2e64d5eac7bd53ed116e3
Tomek Paczkowski <tomek@hauru.eu>
2013-03-25 23:04:05 -0700

    Disable command

--- sprinter/achievements/management/commands/recalculate_achievements.py
+++ sprinter/achievements/management/commands/recalculate_achievements.py
@@ -1,11 +1,4 @@
-from datetime import datetime
-
 from django.core.management.base import BaseCommand, CommandError
-
-from sprinter.achievements.proxies import TicketChangesImporter, GithubImporter
-from sprinter.achievements.models import Achievement
-from sprinter.achievements.processor import process_changes
-from sprinter.userprofile.models import Sprinter
 
 
 class Command(BaseCommand):
@@ -13,16 +6,4 @@ class Command(BaseCommand):
     help = 'Pass trac user and password to recalculate achievements.'
 
     def handle(self, user, password, *args, **options):
-        logins = [sprinter.trac_login for sprinter in Sprinter.objects.all()]
-        github_logins = [sprinter.github_login for sprinter in\
-                Sprinter.objects.all()]
-        start_date = datetime(2013, 2, 23, 15, 0, 0, 0) # TODO: customize date
-
-        proxy = TicketChangesImporter(user=user, password=password,\
-            logins=logins, start_date=start_date)
-        changes = proxy.fetch()
-        github_proxy = GithubImporter(logins=github_logins,\
-                start_date=start_date)
-        github_changes = proxy.fetch()
-        process_changes(changes, github_changes)
-        
+        pass
